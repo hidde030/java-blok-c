3f81f434b807095938bbc48c8777b85d030bf1a0
Hidde Glansdorp <hidde.glansdorp@student.hu.nl>
2022-02-05 00:01:20 +0100

    opdr1 +2

---
+++ college1/opdr2.java
@@ -0,0 +1,13 @@
+/**
+ * opdr2
+ */
+public class opdr2 {
+    public static void main(String[] args){
+        int i = 1;
+        while (i < 11) {
+            System.out.println("Het getal is "+ i);
+            i++;
+            
+        }
+    }
+}
