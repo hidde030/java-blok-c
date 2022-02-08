cf5e91f4f9708034c6db1b41ed53867e8e183191
Hidde Glansdorp <glansdorphidde@gmail.com>
2022-02-08 15:43:38 +0100

    Update

---
+++ college1/opdr4.java
@@ -0,0 +1,13 @@
+public class opdr4 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 39;
+        for(int i = 1; i <= num; ++i)
+        {
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
+
+    }
+}
---
+++ college1/opdr5.java
@@ -0,0 +1,14 @@
+public class opdr5 {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+//            check of number even is
+            if(i % 2 == 0){
+                System.out.println("s");
+            }
+//            Oneven
+            else{
+                System.out.println("ss");
+            }
+        }
+    }
+}
