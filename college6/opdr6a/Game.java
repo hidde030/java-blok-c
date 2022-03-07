5525d589cea07c4320c5df27d93302d3230a0779
Hidde Glansdorp <glansdorphidde@gmail.com>
2022-03-07 13:47:44 +0100

    Update huidigwaarde functie opdr 6a

--- college6/opdr6a/Game.java
+++ college6/opdr6a/Game.java
@@ -21,8 +21,19 @@ public class Game {
      * Methode huidigeWaarde() berekent wat de waarde in het huidige jaar is.
      * 2 games zijn hetzelfde als de naam en het releaseJaar hetzelfde zijn.
      */
-    public void huidigeWaarde() {
-        System.out.println(LocalDate.now());
+    public double huidigeWaarde() {
+
+        int huidigJaar = LocalDate.now().getYear();
+        int jarenOud = huidigJaar - releaseJaar;
+        double kortingsPercentage = 0.7;
+        double huidigeWaarde = 0;
+        for (int i = 0; i < jarenOud+1; i++) {
+            huidigeWaarde = nieuwprijs * (Math.pow(kortingsPercentage, i)) ;
+        }
+        if (jarenOud == 0)
+            return nieuwprijs;
+        else
+            return huidigeWaarde;
     }
 
 }
